e3a3d8e75b650f95f4cc88068d760fa4a998fe48
vivek3009 <vivekpremkumar30@gmail.com>
2018-06-01 11:04:39 +0530

    response message altered

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ public class App {
 	public static void main(String args[]) {
 
 double result = SimpleInterest.calculateSimpleInterest(10000, 5, 7);
-System.out.println("The simple interest is:" + result);  
+System.out.println("The intrest per month is " + result);  
 	}                                                                                                                       public static double calculateSimpleInterest(double amount,
         double years,
         double rate_of_interest) {
