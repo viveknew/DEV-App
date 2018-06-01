f2b5732303375aa54590aca61c2da7bda29ec653
vivek3009 <vivekpremkumar30@gmail.com>
2018-06-01 07:00:38 +0530

    sending to github

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,16 @@
+package com.example.bank;
+
+public class App {
+	
+	public static void main(String args[]) {
+
+double result = SimpleInterest.calculateSimpleInterest(10000, 5, 7);
+System.out.println("The simple interest is:" + result);  
+	}                                                                                                                       public static double calculateSimpleInterest(double amount,
+        double years,
+        double rate_of_interest) {
+    double simple_interest;
+    simple_interest = amount * years * rate_of_interest;
+    return simple_interest;
+} 
+}
