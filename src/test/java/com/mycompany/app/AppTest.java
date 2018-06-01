0eaff4a55287b23695188f50b280b281bf605936
vivek3009 <vivekpremkumar30@gmail.com>
2018-06-01 13:52:19 +0530

    changed to 35000

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,7 +20,7 @@ public class AppTest
     {
         super( testName );
 		double result=App.calculateSimpleInterest(10000,5,7);
-		Assert.assertEquals("Test failed. ", 350000.0, result);   
+   		Assert.assertEquals("Test failed. ", 35000.0, result);   
     }
 
     /**
